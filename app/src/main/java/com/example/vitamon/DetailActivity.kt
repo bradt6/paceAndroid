e9a7651b9c27b72ecf583b195ea9b0fbd178e04e
Brad T <bradthurlow@gmail.com>
2019-06-03 12:24:54 +1000

    added graph for patients

--- app/src/main/java/com/example/vitamon/DetailActivity.kt
+++ app/src/main/java/com/example/vitamon/DetailActivity.kt
@@ -1,10 +1,24 @@
 package com.example.vitamon
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_detail.view.*
+import kotlin.random.Random
 
 class DetailActivity : AppCompatActivity() {
+
+    var heartRateDataList = ArrayList<Int>()
+    var tempDataList = ArrayList<Int>()
+    var o2ListDataList = ArrayList<Int>()
+    var timeList = ArrayList<Double>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +26,120 @@ class DetailActivity : AppCompatActivity() {
 
        val navbarTitle =  intent.getStringExtra("patient_First_Name")
         textView.text = navbarTitle
+
+        for (i in 0..48) {
+            heartRateDataList.add(Random.nextInt(60,150))
+            tempDataList.add(Random.nextInt(25,32))
+            o2ListDataList.add((Random.nextInt(500,600)))
+        }
+        for (i in 0..48) {
+            timeList.add(i.toDouble() / 2.0)
+        }
+        Log.d("HR", heartRateDataList.toString())
+        Log.d("TEMP", tempDataList.toString())
+        Log.d("o2ListData", o2ListDataList.toString())
+        Log.d("TIME", timeList.toString())
+
+        setupBarChartData()
+        setupLineChartData()
+
+    }
+
+    private fun setupBarChartData() {
+        // create BarEntry for Bar Group
+        val bargroup = ArrayList<BarEntry>()
+        bargroup.add(BarEntry(0f, 30f, "0"))
+        bargroup.add(BarEntry(1f, 2f, "1"))
+        bargroup.add(BarEntry(2f, 4f, "2"))
+        bargroup.add(BarEntry(3f, 6f, "3"))
+        bargroup.add(BarEntry(4f, 8f, "4"))
+        bargroup.add(BarEntry(5f, 10f, "5"))
+        bargroup.add(BarEntry(6f, 22f, "6"))
+        bargroup.add(BarEntry(7f, 12.5f, "7"))
+        bargroup.add(BarEntry(8f, 22f, "8"))
+        bargroup.add(BarEntry(9f, 32f, "9"))
+        bargroup.add(BarEntry(10f, 54f, "10"))
+        bargroup.add(BarEntry(11f, 28f, "11"))
+
+        val barGrouping = ArrayList<BarEntry>()
+
+        heartRateDataList.forEachIndexed { i, item ->
+            barGrouping.add(BarEntry(i.toFloat(), item.toFloat()))
+        }
+
+        // creating dataset for Bar Group
+        val barDataSet = BarDataSet(barGrouping, "")
+
+        barDataSet.color = ContextCompat.getColor(this,R.color.amber)
+
+        val data = BarData(barDataSet)
+        barChart.setData(data)
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.labelCount = 11
+        barChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        barChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        barChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        barChart.description.isEnabled = false
+        barChart.animateY(1000)
+        barChart.legend.isEnabled = false
+        barChart.setPinchZoom(true)
+        barChart.data.setDrawValues(false)
+    }
+
+    private fun setupLineChartData() {
+//        val yVals = ArrayList<Entry>()
+//        yVals.add(Entry(0f, 30f, "0"))
+//        yVals.add(Entry(1f, 2f, "1"))
+//        yVals.add(Entry(2f, 4f, "2"))
+//        yVals.add(Entry(3f, 6f, "3"))
+//        yVals.add(Entry(4f, 8f, "4"))
+//        yVals.add(Entry(5f, 10f, "5"))
+//        yVals.add(Entry(6f, 22f, "6"))
+//        yVals.add(Entry(7f, 12.5f, "7"))
+//        yVals.add(Entry(8f, 22f, "8"))
+//        yVals.add(Entry(9f, 32f, "9"))
+//        yVals.add(Entry(10f, 54f, "10"))
+//        yVals.add(Entry(11f, 28f, "11"))
+//
+//        val set1: LineDataSet
+//        set1 = LineDataSet(yVals, "DataSet 1")
+
+        val yHRVals = ArrayList<Entry>()
+
+        heartRateDataList.forEachIndexed { i, item ->
+            yHRVals.add(BarEntry(i.toFloat(), item.toFloat()))
+        }
+        val set1: LineDataSet
+        set1 = LineDataSet(yHRVals, "DataSet 1")
+
+        // set1.fillAlpha = 110
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        // set1.enableDashedLine(5f, 5f, 0f);
+        // set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.color = Color.BLUE
+        set1.setCircleColor(Color.BLUE)
+        set1.lineWidth = 1f
+        set1.circleRadius = 3f
+        set1.setDrawCircleHole(false)
+        set1.valueTextSize = 0f
+        set1.setDrawFilled(false)
+
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(set1)
+        val data = LineData(dataSets)
+
+        // set data
+        HRLineChart.setData(data)
+        HRLineChart.description.isEnabled = false
+        HRLineChart.legend.isEnabled = false
+        HRLineChart.setPinchZoom(true)
+        HRLineChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        HRLineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        HRLineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        //lineChart.setDrawGridBackground()
+        HRLineChart.xAxis.labelCount = 11
+        HRLineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 }
