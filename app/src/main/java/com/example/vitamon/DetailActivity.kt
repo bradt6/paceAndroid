deb12d693e4dfba7c0d5d0036cf8115b94c1305b
Brad T <bradthurlow@gmail.com>
2019-06-04 19:38:07 +1000

    added titles to the grapgh

--- app/src/main/java/com/example/vitamon/DetailActivity.kt
+++ app/src/main/java/com/example/vitamon/DetailActivity.kt
@@ -19,6 +19,7 @@ class DetailActivity : AppCompatActivity() {
     var tempDataList = ArrayList<Int>()
     var o2ListDataList = ArrayList<Int>()
     var timeList = ArrayList<Double>()
+    var bloodPreasureList = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +32,7 @@ class DetailActivity : AppCompatActivity() {
             heartRateDataList.add(Random.nextInt(60,150))
             tempDataList.add(Random.nextInt(25,32))
             o2ListDataList.add((Random.nextInt(500,600)))
+            bloodPreasureList.add((Random.nextInt(70,180)))
         }
         for (i in 0..48) {
             timeList.add(i.toDouble() / 2.0)
@@ -41,8 +43,9 @@ class DetailActivity : AppCompatActivity() {
         Log.d("TIME", timeList.toString())
 
         setupBarChartData()
-        setupLineChartData()
-        setupLineChartData2()
+        setupLineChartDataHR()
+        setupLineChartDataO2()
+        setupLineChartDataBloodPreasure()
 
     }
 
@@ -73,7 +76,7 @@ class DetailActivity : AppCompatActivity() {
         barChart.data.setDrawValues(false)
     }
 
-    private fun setupLineChartData() {
+    private fun setupLineChartDataHR() {
 
         val yHRVals = ArrayList<Entry>()
 
@@ -114,7 +117,7 @@ class DetailActivity : AppCompatActivity() {
         HRLineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 
-    private fun setupLineChartData2() {
+    private fun setupLineChartDataO2() {
 
         val y02Vals = ArrayList<Entry>()
 
@@ -143,15 +146,56 @@ class DetailActivity : AppCompatActivity() {
         val data = LineData(dataSets)
 
         // set data
-        trial.setData(data)
-        trial.description.isEnabled = false
-        trial.legend.isEnabled = false
-        trial.setPinchZoom(true)
-        trial.xAxis.enableGridDashedLine(5f, 5f, 0f)
-        trial.axisRight.enableGridDashedLine(5f, 5f, 0f)
-        trial.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        sp02Line.setData(data)
+        sp02Line.description.isEnabled = false
+        sp02Line.legend.isEnabled = false
+        sp02Line.setPinchZoom(true)
+        sp02Line.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        sp02Line.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        sp02Line.axisLeft.enableGridDashedLine(5f, 5f, 0f)
         //lineChart.setDrawGridBackground()
-        trial.xAxis.labelCount = 11
-        trial   .xAxis.position = XAxis.XAxisPosition.BOTTOM
+        sp02Line.xAxis.labelCount = 11
+        sp02Line.xAxis.position = XAxis.XAxisPosition.BOTTOM
+    }
+
+    private fun setupLineChartDataBloodPreasure() {
+
+        val yBloodPreasureVals = ArrayList<Entry>()
+
+        bloodPreasureList.forEachIndexed { i, item ->
+            yBloodPreasureVals.add(BarEntry(i.toFloat(), item.toFloat()))
+        }
+        val set1: LineDataSet
+        set1 = LineDataSet(yBloodPreasureVals, "DataSet 1")
+
+        // set1.fillAlpha = 110
+        // set1.setFillColor(Color.RED);
+
+        // set the line to be drawn like this "- - - - - -"
+        // set1.enableDashedLine(5f, 5f, 0f);
+        // set1.enableDashedHighlightLine(10f, 5f, 0f);
+        set1.color = Color.RED
+        set1.setCircleColor(Color.RED)
+        set1.lineWidth = 1f
+        set1.circleRadius = 3f
+        set1.setDrawCircleHole(false)
+        set1.valueTextSize = 0f
+        set1.setDrawFilled(false)
+
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(set1)
+        val data = LineData(dataSets)
+
+        // set data
+        bloodPLine.setData(data)
+        bloodPLine.description.isEnabled = false
+        bloodPLine.legend.isEnabled = false
+        bloodPLine.setPinchZoom(true)
+        bloodPLine.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        bloodPLine.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        bloodPLine.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        //lineChart.setDrawGridBackground()
+        bloodPLine.xAxis.labelCount = 11
+        bloodPLine.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 }
